c37171916205481a987eea226eb79edcb7977f70
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-10-09 09:29:08 -0400

    temp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double tempC = 0;
+        double tempF = 0;
+        boolean done = false;
+
+
+        do {
+            //prompt
+            System.out.println("Please enter a temperature in Celcius: ");
+            //input
+            if (scan.hasNextDouble()) {
+                tempC = scan.nextDouble();
+                scan.nextLine();
+                done = true;
+            }
+            else {
+                System.out.println("You have entered an invalid Temp. Try again");
+                scan.nextLine();
+            }
+        } while (!done);
+        tempF = (tempC * 9/5) + 32;
+        System.out.printf("\n%-10s %5.2f", "Celcius:", tempC);
+        System.out.printf("\n%-10s %5.2f", "Fahrenheit:", tempF);
+
+
+
+
+
     }
 }
